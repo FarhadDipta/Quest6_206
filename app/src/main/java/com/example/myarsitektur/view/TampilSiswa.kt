9f84544b1301ff21b7f4c9ecb54bb8b312e8a8ff
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2025-11-20 18:02:47 +0700

    create TampilSiswa composable function header

--- app/src/main/java/com/example/myarsitektur/view/TampilSiswa.kt
+++ app/src/main/java/com/example/myarsitektur/view/TampilSiswa.kt
@@ -23,3 +23,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.myarsitektur.R
 import com.example.myarsitektur.model.Siswa
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilSiswa(
+    statusUiSiswa: Siswa,
+    onBackButtonClicked: () -> Unit,
+) {
