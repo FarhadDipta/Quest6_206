8c49d6c235c8c2e86778e75095c23f9cd8ec16a0
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2025-11-20 17:56:51 +0700

    Import Lanjutan + Resource

--- app/src/main/java/com/example/myarsitektur/view/FormSiswa.kt
+++ app/src/main/java/com/example/myarsitektur/view/FormSiswa.kt
@@ -17,3 +17,14 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
