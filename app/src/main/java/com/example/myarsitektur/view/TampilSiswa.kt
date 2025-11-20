04447bd3dab4fd419197a513973ab76203745ace
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2025-11-20 18:02:29 +0700

    include font sizing utilities and Siswa model

--- app/src/main/java/com/example/myarsitektur/view/TampilSiswa.kt
+++ app/src/main/java/com/example/myarsitektur/view/TampilSiswa.kt
@@ -19,3 +19,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.myarsitektur.R
+import com.example.myarsitektur.model.Siswa
